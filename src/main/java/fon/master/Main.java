0d0688526acc9aba7dd4d45602165cb130a5fccc
mferketic <marioskola1990@gmail.com>
2023-04-01 23:53:44 +0200

    Implement Docker Hub support

--- src/main/java/fon/master/Main.java
+++ src/main/java/fon/master/Main.java
@@ -2,6 +2,6 @@ package fon.master;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Docker!");
+        System.out.println("Hello Docker Hub!");
     }
 }
